36d010bbef6f7e1f2401bd069058af382b66784f
CHATHURANGA KHD <144873531+Deneth0077@users.noreply.github.com>
2024-03-09 19:39:36 +0530

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+const animate = ScrollReveal ({
+    origin:'top',
+    distance: "60px",
+    duration: "2500",
+    delay: "400",
+});
+
+animate.reveal(".nav");
+animate.reveal(".home-text", { origin: "left"});
+animate.reveal(".home-img", { origin: "bottom"});
+animate.reveal(".ser-box, .product-box, .team-box, .book-data", {  intervel: 100});
